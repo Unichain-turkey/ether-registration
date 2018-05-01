2ecbf36c74575aed84a02077e70faaa115636ab0
Mevlana Ayas <mevlanaayas@gmail.com>
2018-05-01 16:06:56 +0300

    küçük ayrıntılar

--- dapp/src/components/Header.vue
+++ dapp/src/components/Header.vue
@@ -2,12 +2,12 @@
   <div>
     <nav class="navbar sticky-top navbar-light bg-light">
       <a class="navbar-brand" href="#">Registhereum</a>
-      <b>Balance: {{$store.getters.balance}},000 ETH</b>
-      <b>Address: {{$store.getters.coinbase}}</b>
+      <b>Balance: {{$store.getters.balance}} ETH</b>
+      <b>Address: {{$store.getters.currentAddress}}</b>
       <b>Network: {{$store.getters.network}}</b>
       <b></b>
     </nav>
-    <div class="jumbotron jumbotron-fluid text-center">
+    <div class="jumbotron jumbotron-fluid text-center bg-primary">
       <div class="container-fluid">
         <h1 class="display-4">Hello UniChain</h1>
         <p class="lead ">This is an Activity registration app by using smart contracts.</p>
