aa76e320e0d1de9122856157bf04f0b6c5b9b204
mevlanaayas <mevlanaayas@gmail.com>
2018-07-01 12:15:13 +0300

    details rearrange

--- dapp/src/components/ActivityDetail.vue
+++ dapp/src/components/ActivityDetail.vue
@@ -1,28 +1,34 @@
 <template>
 
   <div class="container">
-    <div class="item  col-xs-12 col-lg-12 list-group-item"  track-by="id" >
+    <div class="item  col-xs-12 col-lg-12 list-group-item"  track-by="id" style="border: 0">
       <div class="thumbnail">
-        <div class="row">
-        <div class="col-md-8 flex-first">
-          <h4 class="display-4 text-center text-uppercase" >
-            {{ item['0'] }}</h4>
-          <p class>{{item['7']}}</p>
-          <p class="group inner list-group-item-text"  v-bind:style="{ color: bgColor}">
-            {{getActive}}</p>
-          <p class="group inner list-group-item-text">
-            Price : {{getPrice}}</p>
-          <p class="group inner list-group-item-text">
-            Registered : {{item['4']}}</p>
-          <p class="group inner list-group-item-text">
-            Limit:  {{item['5']}}</p>
-          <p class="group inner list-group-item-text">
-            Date:  {{when}}</p>
-          <p class="group inner list-group-item-text">
-            Owner: {{item['1']}}</p>
+        <div class="card" style="border: 0">
+          <div class="card-body">
+            <img class="card-img img-fluid rounded mx-auto d-block" :src="getImageUrl(item['8'])" alt="Card image" style="height: 75%; width: 75%" >
+          </div>
         </div>
-          <img class="col-md-4 flex-last" :src="getImageUrl(item['8'])" />
-      </div>
+        <div class="row">
+          <div class="col-md flex-first">
+            <h3 class="text-center text-uppercase">{{ item['0'] }}</h3>
+            <p class="group inner list-group-item-text">
+              Description: {{item['7']}}
+            </p>
+            <p class="group inner list-group-item-text"  v-bind:style="{ color: bgColor}">
+              <span style="color: black;">Status: </span>{{getActive}}
+            </p>
+            <p class="group inner list-group-item-text">
+              Price : {{getPrice}}</p>
+            <p class="group inner list-group-item-text">
+              Registered User Count : {{item['4']}}</p>
+            <p class="group inner list-group-item-text">
+              Student Limit:  {{item['5']}}</p>
+            <p class="group inner list-group-item-text">
+              Activity Date:  {{when}}</p>
+            <p class="group inner list-group-item-text">
+              Activity Creator Address: {{item['1']}}</p>
+          </div>
+        </div>
       </div>
       <br>
       <hr>
