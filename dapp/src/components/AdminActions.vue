eb0f8c666539e984719a699043530dcb80e3a7a8
mevlanaayas <mevlanaayas@gmail.com>
2018-07-01 10:52:59 +0300

    admin actions

--- dapp/src/components/AdminActions.vue
+++ dapp/src/components/AdminActions.vue
@@ -1,34 +1,33 @@
 <template>
   <div>
     <div class="progress" v-if="pending">
-      <p>Sending transaction</p>
       <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
     </div>
     <h4 class="display-5">Admin Actions</h4>
     <br>
     <div class="container">
       <div class="input-group">
-        <input type="text" class="form-control col-md-6"  v-model="payCode" placeholder="Secret Code" aria-describedby="btnGroupAddon">
+        <input type="text" class="form-control col-md-7"  v-model="payCode" placeholder="Secret Code 0x00000000000" aria-describedby="btnGroupAddon">
         <button  v-on:click="setPayCode" class="btn btn-outline-success btn-block col-md-3">Send Code</button>
       </div>
       <br/>
       <div class="input-group">
-        <input type="text" class="form-control col-md-6"  v-model="active" placeholder="True or False" aria-describedby="btnGroupAddon">
-        <button  v-on:click="setActive" class="btn btn-outline-success btn-block col-md-3">Set</button>
+        <button  v-on:click="activate" class="btn btn-outline-success btn-block col-md-5">Free</button>
+        <button  v-on:click="deactivate" class="btn btn-outline-danger btn-block col-md-5" style="margin-top: 0">For Pay</button>
       </div>
       <br/>
       <div class="input-group">
-        <input type="text" class="form-control col-md-6"  v-model="newOwner" placeholder="0x3TH3R3UMADDR3SS" aria-describedby="btnGroupAddon">
-        <button  v-on:click="changeOwner" class="btn btn-outline-success btn-block col-md-3">Say goodbye :(</button>
+        <input type="text" class="form-control col-md-7"  v-model="newOwner" placeholder="0x3TH3R3UMADDR3SS" aria-describedby="btnGroupAddon">
+        <button  v-on:click="changeOwner" class="btn btn-outline-success btn-block col-md-3">Leave Activity</button>
       </div>
       <br/>
       <div class="input-group">
-        <input type="text" class="form-control col-md-6"  v-model="user" placeholder="I have a high ground" aria-describedby="btnGroupAddon">
-        <button  v-on:click="deleteUser" class="btn btn-outline-success btn-block col-md-3">Get out</button>
+        <input type="text" class="form-control col-md-7"  v-model="user" placeholder="I have a high ground" aria-describedby="btnGroupAddon">
+        <button  v-on:click="deleteUser" class="btn btn-outline-success btn-block col-md-3">Kick User</button>
       </div>
       <br/>
       <div class="input-group">
-        <input type="text" class="form-control col-md-6"  v-model="userlist" placeholder="Who is there" aria-describedby="btnGroupAddon">
+        <input type="text" class="form-control col-md-7"  v-model="userlist" placeholder="Who is there" aria-describedby="btnGroupAddon">
         <button  v-on:click="getUserList" class="btn btn-outline-success btn-block col-md-3">Get list</button>
       </div>
     </div>
@@ -45,6 +44,7 @@ export default {
       newOwner: '',
       user: '',
       payCode: '',
+      currentPayCode: '',
 
       name: null,
       surname: null,
@@ -63,19 +63,6 @@ export default {
     this.coinbase = this.$store.getters.currentAddress
   },
   methods: {
-    adminAct: function (e) {
-      console.log(this.contractInstance)
-      console.log(this.price)
-      const temp = this.contractInstance.methods.registerToActivity(
-        this.address,
-        this.email
-      ).send(
-        {value: this.$options.filters.toWei('0.1'), from: this.coinbase})
-      temp.then(function (error, value) {
-        console.log(error)
-        console.log(value)
-      })
-    },
     changeOwner: function (e) {
       const temp = this.contractInstance.methods.transferOwnershipActivity(
         this.coinbase,
@@ -102,10 +89,21 @@ export default {
         self.pending = false
       })
     },
-    setActive: function (e) {
-      const temp = this.contractInstance.methods.transferOwnershipActivity(
-        this.coinbase,
-        this.newOwner
+    activate: function (e) {
+      const temp = this.contractInstance.methods.openPayActive(
+        this.coinbase
+      ).send(
+        {from: this.coinbase, gas: 4500000})
+      this.pending = true
+      var self = this
+      temp.then(function (value) {
+        console.log(value)
+        self.pending = false
+      })
+    },
+    deactivate: function (e) {
+      const temp = this.contractInstance.methods.closePayActive(
+        this.coinbase
       ).send(
         {from: this.coinbase, gas: 4500000})
       this.pending = true
@@ -116,9 +114,9 @@ export default {
       })
     },
     deleteUser: function (e) {
-      const temp = this.contractInstance.methods.transferOwnershipActivity(
+      const temp = this.contractInstance.methods.deleteParticipiant(
         this.coinbase,
-        this.newOwner
+        this.payCode
       ).send(
         {from: this.coinbase, gas: 4500000})
       this.pending = true
@@ -129,7 +127,7 @@ export default {
       })
     },
     getUserList: function (e) {
-      const temp = this.contractInstance.methods.transferOwnershipActivity(
+      const temp = this.contractInstance.methods.getParticipiant(
         this.coinbase,
         this.newOwner
       ).send(
